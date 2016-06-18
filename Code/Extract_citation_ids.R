35938c963b6a009f508a39635e52e74e6a5449f8
Meredith <alun.g.m@gmail.com>
2016-06-18 08:33:09 +0100

    ...

--- Code/Extract_citation_ids.R
+++ Code/Extract_citation_ids.R
@@ -5,11 +5,13 @@
 # Retreives basic info about the patents cited by that document, returned as a matrix
 # Documents any structural attributes which are not as expected (and therefore elements have been thrown away)
 parse_xml <- function(doc.txt) {
+    library(xml2)
     
     doc.xml <- read_xml(doc.txt)
     y <- xml_find_all(doc.xml, "//us-bibliographic-data-grant/publication-reference/document-id")
     patent.info <- as.character(xml_contents(xml_children(y)))
     names(patent.info) <- xml_name((xml_children(y)))
+    parent.patent <- patent.info["doc-number"]
     
     citations <- xml_find_all(doc.xml, "//us-references-cited/us-citation/patcit/document-id")
     attr <- lapply(citations, function(x) xml_name(xml_children(x)))       
@@ -19,8 +21,7 @@ parse_xml <- function(doc.txt) {
     # If incorrectly named, report the name and values
     allowed.attr <- c("country", "doc-number", "kind", "name", "date")
     citation.info <- matrix(NA, ncol = length(values), nrow = 5, dimnames = list(allowed.attr, NULL))
-
-    #### TODO
+    
     for (i in seq_along(attr)) {
         attr.i <- attr[[i]]
         values.i <- values[[i]]
@@ -32,47 +33,148 @@ parse_xml <- function(doc.txt) {
         })
         values.i <- values.i[!is.na(mat.index)]
         mat.index <- na.omit(mat.index)
-        citation.info[mat.index,1] <- values.i
+        citation.info[mat.index,i] <- values.i
     }
     
-    parent.patent <- patent.info["doc-number"]
-    citation.info <- rbind(citation.info, parent.patent)
-    
-    incorrect.attr <- attr[attr != allowed.attr]
+    # Record a list of the attributes which did not fit with the specified structure
+    unlisted.attr <- unlist(attr)
+    incorrect.attr <- unlisted.attr[!(unlisted.attr %in% allowed.attr)]
     if (length(incorrect.attr) > 1) incorrect.attr <- cbind(incorrect.attr, parent.patent)
+    
+    #citation.info <- rbind(citation.info, parent.patent) #///////////////////
+    citation.info.old <- citation.info
+    if (dim(citation.info)[2] > 0) {
+        citation.info <- tryCatch(rbind(citation.info, parent.patent), warning = function(w) w)
+    }
+    else citation.info <- NULL
+    if (is(citation.info, "warning")) browser()
     
     return(list(patent.info = patent.info, citation.info = t(citation.info), incorrect.attr = incorrect.attr))
 }
 
 
-
 # This function reads in a data file and returns a list of citations for each patent document 
 parse_file <- function(file.txt) {
+    library(readr)
     # Read a file into R
-    lines <- readLines(file.txt)
+    lines <- read_lines(file.txt)
     # Create an index to seperate the different xml documents within.
-    xmltags <- grep("<?xml", lines)
+    xmltags <- grep("<?xml", lines, fixed = T)
     
     # Loop over each xml document in the file
     citation.ids.file <- list()
-    citation.info.file <- NA
-    patent.info.file <- NA
-    incorrect.attr.file <- NA
-    for (i in 2:2){ #length(xmltags)) {
+    citation.info.file <- NULL
+    patent.info.file <- NULL
+    incorrect.attr.file <- NULL
+    for (i in 2:length(xmltags)) {
         # Define the xml document 
         xml <- paste(lines[xmltags[i-1]:(xmltags[i]-1)], collapse = "")
         
         parsed.xml <- try(parse_xml(xml), silent = T)
+
         # Error catching in case no citations returned by parse_xml
         if (class(parsed.xml) !=  "try-error") {
-            citation.info.file <- rbind(citation.info.file, parsed.xml$citation.info)
+            citaiton.info.file.old <- citation.info.file
+            citation.info.file <- tryCatch(rbind(citation.info.file, parsed.xml$citation.info), error = function(e) e)
+            if (is(citation.info.file, "error")) browser()
             patent.info.file <- rbind(patent.info.file, parsed.xml$patent.info)
-            incorrect.attr.file <- rbind(incorrect.attr.file, parsed.xml$incorrect.attr)
+            if (length(parsed.xml$incorrect.attr) > 0) {
+                incorrect.attr.file <- rbind(incorrect.attr.file, parsed.xml$incorrect.attr)
+            }
         }
     }
     gc()
     return(list(citations = citation.info.file, patent = patent.info.file, incorrect.attr = incorrect.attr.file))
 }
 
-test <- parse_file("DataFiles/testdata.xml")
-# parse_xml(xml1)
+
+# This function cycles through a directory (year) and parses it acording to the above two functions and saves them 
+parse_directory <- function(read.dir, write.dir) {
+    # Define the list of files which needss to be processed
+    # Read files in directory
+    files <- list.files(read.dir)
+    require(stringr)
+    files.xml <- str_subset(files, "\\.xml")
+    # Read files which have already been processed
+    files.processed <- list.files(write.dir)
+    if (length(files.processed) > 0) {
+        # Generate a regex expression for finding those files in the read directory
+        files.processed <- str_replace(files.processed, "_(incorrectattr|misspat|patent|citations)\\.csv", "")
+        print(sprintf("Files already processed: %i", length(files.processed)/4))
+        files.processed.regex <-  paste0(files.processed, "|", collapse = "")
+        files.processed.regex <-  substr(files.processed.regex, 1, nchar(files.processed.regex)-1)
+        # Exclude already processed files from the list
+        files.xml <- files.xml[!str_detect(files.xml, files.processed.regex)]
+    }    
+    
+    files.root <- str_replace(files.xml, "\\.xml", "")    
+    file.paths.xml <- paste0(read.dir, "/", files.xml)
+    file.paths.txt <- paste0(read.dir, "/", files.root, "lst.txt")
+    for (i in seq_along(files.xml)) {
+        print(sprintf("Processing: %s (%i/%i)", files.root[i], i, length(files.xml)))
+        t <- Sys.time()
+        parsed <- parse_file(file.paths.xml[i])
+        print(paste("Time to process: ", Sys.time() - t))
+        print(paste("Estimated time rematining:", (length(files.xml) - i - 1)*(Sys.time()-t)))
+        patent.list <- read_lines(file.paths.txt[i])
+        patents.missing <- patent.list[!(patent.list %in% parsed$patent[,2])]
+        
+        # Save files
+        print(paste("Writing: ", files.root[i]))
+        if (!dir.exists(write.dir)) dir.create(write.dir, recursive = T)
+        write_csv(as.data.frame(parsed$citations), paste0(write.dir, "/", files.root[i], "_citations.csv"))
+        write_csv(as.data.frame(parsed$patent), paste0(write.dir, "/", files.root[i], "_patent.csv"))
+        write_csv(as.data.frame(parsed$incorrect.attr), paste0(write.dir, "/", files.root[i], "_incorrectattr.csv"))
+        write_csv(as.data.frame(patents.missing), paste0(write.dir, "/", files.root[i], "_misspat.csv"))
+        gc()
+    }
+}
+
+
+parse_xml_2 <- function(doc.txt) {
+    library(xml2)
+    
+    doc.xml <- read_xml(doc.txt)
+    y <- xml_find_all(doc.xml, "//us-bibliographic-data-grant/publication-reference/document-id")
+    patent.info <- as.character(xml_contents(xml_children(y)))
+    names(patent.info) <- xml_name((xml_children(y)))
+    parent.patent <- patent.info["doc-number"]
+    
+    citations <- xml_find_all(doc.xml, "//us-references-cited/us-citation/patcit/document-id")
+    attr <- lapply(citations, function(x) xml_name(xml_children(x)))       
+    values <- lapply(citations, function(x) as.character(xml_contents(xml_children(x))))
+    
+    # Build a matrix from the return values where they are correctly named
+    # If incorrectly named, report the name and values
+    allowed.attr <- c("country", "doc-number", "kind", "name", "date")
+    citation.info <- matrix(NA, ncol = length(values), nrow = 5, dimnames = list(allowed.attr, NULL))
+    
+    for (i in seq_along(attr)) {
+        attr.i <- attr[[i]]
+        values.i <- values[[i]]
+        # Create an index relating the attributes to the columns of the matrix
+        mat.index <- sapply(attr.i, function(x) {
+            index <- which(allowed.attr == x)
+            if (length(index) < 1) return(NA)
+            return(index)
+        })
+        values.i <- values.i[!is.na(mat.index)]
+        mat.index <- na.omit(mat.index)
+        citation.info[mat.index,i] <- values.i
+    }
+    
+    # Record a list of the attributes which did not fit with the specified structure
+    unlisted.attr <- unlist(attr)
+    incorrect.attr <- unlisted.attr[!(unlisted.attr %in% allowed.attr)]
+    if (length(incorrect.attr) > 1) incorrect.attr <- cbind(incorrect.attr, parent.patent)
+    
+    #citation.info <- rbind(citation.info, parent.patent) #///////////////////
+    citation.info.old <- citation.info
+    if (dim(citation.info)[2] > 0) {
+        citation.info <- tryCatch(rbind(citation.info, parent.patent), warning = function(w) w)
+    }
+    else citation.info <- NULL
+    if (is(citation.info, "warning")) browser()
+    
+    return(list(patent.info = patent.info, citation.info = t(citation.info), incorrect.attr = incorrect.attr))
+}
---
+++ Code/extract_ids_script.R
@@ -0,0 +1,9 @@
+# Extract for a specific year
+read.dir <- "DataFiles/Raw/2015"
+write.dir <- "DataFiles/Processed/2015"
+parse_directory <- parse_directory(read.dir, write.dir)
+
+# Test xml parsing for each year to see results
+
+
+#
