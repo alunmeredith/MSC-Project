96284244dc3a6d46634765cb544167c8c1061ab7
Meredith <alun.g.m@gmail.com>
2016-08-10 09:40:25 +0100

    aa

--- Code/Cleaning.R
+++ Code/Cleaning.R
@@ -17,6 +17,7 @@ cleaning <- function(input.dir = "Processed2", output.dir = "Cleaned2",
     library(readr)
     library(dplyr)
     library(lubridate)
+    library(stringr)
     
     # Define input and output directories
     input.dir.pat <- paste0("../DataFiles/", input.dir, "/patent")
@@ -24,69 +25,70 @@ cleaning <- function(input.dir = "Processed2", output.dir = "Cleaned2",
     # Create directory to save data to
     dir.create(output.dir.pat, recursive = TRUE, showWarnings = FALSE)
     
-    # Clean Patent files
-    patent.files <- list.files(input.dir.pat, full.names = T)
-    patent.cat <- NULL
-    for (file in patent.files) {
-        # Add Column Names
-        df <- read_csv(file, col_names = T, col_types = "cdccd")
-        # Replace duplicated entries
-        df <- unique(df)
-        # Convert character date to a posixct class object
-        df$Date2 <- ymd(df$Date)
-        # If the year is 1976:2001 (txt files parsed) then remove a digit from the end of patent number
-        # yr <- stringr::str_extract(file, "[0-9]{4}.csv$") %>% substring(1,4) %>% as.numeric() # extract year
-        # if (yr %in% 1976:2001) {
-        #     df$Patent <- sapply(df$Patent, function(x) substring(x, 1, nchar(x) - 1))
-        # }
-        # Remove the 0s from text patent numbers
-        type_txt <- stringr::str_extract(df$Patent, "^(D|RE|PP|H|T)")
-        type_txt[is.na(type_txt)] <- ""
-        rem0_txt <- sub("^(D|RE|PP|H|T)*0+", "", df$Patent)
-        df$Patent <- paste0(type_txt, rem0_txt)
-        # Concatonate Patents
-        patent.cat <- bind_rows(patent.cat, df)
-        # Write to file
-        file.path <- sub(input.dir, output.dir, file)
-        write_csv(df, file.path)
-    }
-    write_csv(patent.cat, paste0("../DataFiles/", output.dir, "/patent_cat.csv"))
-
-    #Clear environments
-    rm(patent.cat, df)
-    
-    input.dir.cit <- paste0("../DataFiles/", input.dir, "/citation")
-    output.dir.cit <- paste0("../DataFiles/", output.dir, "/citation")
-    dir.create(output.dir.cit, recursive = TRUE, showWarnings = FALSE)
-    
-    # # Clean Citation Files
-    # citation.files <- list.files(input.dir.cit, full.names = T)
-    # for (file in citation.files[11:11]) {
-    #     gc()
-    #     print(file)
+    # # Clean Patent files
+    # patent.files <- list.files(input.dir.pat, full.names = T)
+    # patent.cat <- NULL
+    # for (file in patent.files) {
     #     # Add Column Names
-    #     df <- read_csv(file, col_names = T, col_types = citation.coltypes)
-    #     # Remove Duplicated Entries
+    #     df <- read_csv(file, col_names = T, col_types = "cdccd")
+    #     # Replace duplicated entries
     #     df <- unique(df)
+    #     # Convert character date to a posixct class object
+    #     df$Date2 <- ymd(df$Date)
     #     # If the year is 1976:2001 (txt files parsed) then remove a digit from the end of patent number
-    #     yr <- stringr::str_extract(file, "[0-9]{4}.csv$") %>% substring(1,4) %>% as.numeric() # extract year
-    #     if (yr %in% 1976:2001) {
-    #         df$Patent <- sapply(df$Patent, function(x) substring(x, 1, nchar(x) - 1)) 
-    #     }
+    #     # yr <- stringr::str_extract(file, "[0-9]{4}.csv$") %>% substring(1,4) %>% as.numeric() # extract year
+    #     # if (yr %in% 1976:2001) {
+    #     #     df$Patent <- sapply(df$Patent, function(x) substring(x, 1, nchar(x) - 1))
+    #     # }
     #     # Remove the 0s from text patent numbers
     #     type_txt <- stringr::str_extract(df$Patent, "^(D|RE|PP|H|T)")
     #     type_txt[is.na(type_txt)] <- ""
     #     rem0_txt <- sub("^(D|RE|PP|H|T)*0+", "", df$Patent)
     #     df$Patent <- paste0(type_txt, rem0_txt)
-    #     # To reduce size of file convert patent to factor
-    #     df$Patent <- as.factor(df$Patent)
-    #     # Convert character date to a posixct class object
-    #     # Day not recorded "00" so replaced with "01" to be valid posixct objects
-    #     df$Date2 <- gsub('.{2}$', '01', df$Date)
-    #     df$Date2 <- ymd(df$Date2)
+    #     # Concatonate Patents
+    #     patent.cat <- bind_rows(patent.cat, df)
     #     # Write to file
     #     file.path <- sub(input.dir, output.dir, file)
     #     write_csv(df, file.path)
-    #     rm(df)
     # }
+    # write_csv(patent.cat, paste0("../DataFiles/", output.dir, "/patent_cat.csv"))
+
+    #Clear environments
+    #rm(patent.cat, df)
+    
+    input.dir.cit <- paste0("../DataFiles/", input.dir, "/citation")
+    output.dir.cit <- paste0("../DataFiles/", output.dir, "/citation")
+    dir.create(output.dir.cit, recursive = TRUE, showWarnings = FALSE)
+    
+    # Clean Citation Files
+    citation.files <- list.files(input.dir.cit, full.names = T)
+    for (file in citation.files[15:15]) {
+        gc()
+        print(file)
+        # Add Column Names
+        df <- read_csv(file, col_names = T, col_types = citation.coltypes)
+        # Remove Duplicated Entries
+        df <- unique(df)
+        # # If the year is 1976:2001 (txt files parsed) then remove a digit from the end of patent number
+        # yr <- stringr::str_extract(file, "[0-9]{4}.csv$") %>% substring(1,4) %>% as.numeric() # extract year
+        # if (yr %in% 1976:2001) {
+        #     df$Patent <- sapply(df$Patent, function(x) substring(x, 1, nchar(x) - 1))
+        # }
+        # Remove the 0s from text patent numbers
+        type_txt <- str_extract(df$Patent, "^(D|RE|PP|H|T)")
+        type_txt[is.na(type_txt)] <- ""
+        rem0_txt <- sub("^(D|RE|PP|H|T)*(0|\\.)+", "", df$Patent)
+        rem0_txt <- str_trim(rem0_txt)
+        df$Patent <- paste0(type_txt, rem0_txt)
+        # To reduce size of file convert patent to factor
+        df$Patent <- as.factor(df$Patent)
+        # Convert character date to a posixct class object
+        # Day not recorded "00" so replaced with "01" to be valid posixct objects
+        df$Date2 <- gsub('.{2}$', '01', df$Date)
+        df$Date2 <- ymd(df$Date2)
+        # Write to file
+        file.path <- sub(input.dir, output.dir, file)
+        write_csv(df, file.path)
+        rm(df)
+    }
 }
