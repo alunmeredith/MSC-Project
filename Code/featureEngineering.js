4d162ecf05254ac7178a6ef3aafa7098f6a35b49
Meredith <alun.g.m@gmail.com>
2016-08-17 23:31:25 +0100

    Cleaned code directory a bit, moving old and redundate scripts to OldCode

--- Code/featureEngineering.js
+++ Code/featureEngineering.js
@@ -192,55 +192,93 @@ db.patentsJoined.aggregate([
     	{$out: "ExaminerOtherScatterplot"}
 	])
 
-// ---------------------------------------------------------------------------
-// Map reduce to count order of citations (37.25mins)
-// ---------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// Adds Patent Date (and Date2) to each citation
+// --------------------------------------------------------------------------------------------
 
-var mapper = function() {
-	var key = this.Citation;
-	var value = {
-		order: 1,
-		orderExaminer: 0,
-		orderOther: 0
+var totalPatents = db.patentsOld.count();
+var processed = 0;
+var then = new Date();
+db.patents.find().noCursorTimeout().forEach(function (patentDoc){
+	db.citations.update(
+		{
+			Patent: patentDoc.Patent,
+			PatentDate: {$exists: false}
+		},
+		{$set: {PatentDate: patentDoc.Date, PatentDate2: patentDoc.Date2}},
+		{multi: true});
+	
+	processed += 1;
+	if (processed % 10000 == 0) {
+		now = new Date();-
+		print("Processed: ", processed, "/", totalPatents);
+		print("Time Remaining: ", (totalPatents - processed) * (now - then)/(10000*1000*60));
+		then = now;
 	};
-	if (this.CitedBy == "cited by examiner") {
-		value.orderExaminer = 1;
-	} else {
-		value.orderOther = 1;
-	}
-	emit(key, value);
-};
-
-var reducer = function(keyPatent, valueCitedby) {
-	var citEx = 0;
-	var ord = 0;
-	var citOth = 0;
-	for (var i = valueCitedby.length - 1; i >= 0; i--) {
-		citEx += valueCitedby[i].orderExaminer;
-		ord += valueCitedby[i].order;
-		citOth += valueCitedby[i].orderOther;
-	}
-	return {order: ord, orderExaminer: citEx, orderOther: citOth};
-};
-
-db.citations.mapReduce(
-	mapper,
-	reducer,
-	{
-		out: "citationOrders"
-	}
-);
-
-// ---------------------------------------------------------------------------
-// Create CitationsJoined which adds "PatentDate"
-// ---------------------------------------------------------------------------
-// db.citationsOld.find({ Date: { $gt: 18000000, $lte: 19000000}}).count()
-db.citationsOld.find({ Date: { $gt: 18000000, $lte: 19000000}}).noCursorTimeout().forEach(function (citationDoc){
-	var patentDoc = db.patentsOld.findOne({Patent: citationDoc.Patent});
-	if (patentDoc != null) {
-		citationDoc.PatentDate = patentDoc.Date2;
-		var msDiff = Date.parse(patentDoc.Date2) - Date.parse(citationDoc.Date2)
-		citationDoc.DayLag = msDiff/86400000 
-	  	db.citationsOld.save(citationDoc);
-	}
 });
+print("Processed All Patents-----------------------------------")
+
+// --------------------------------------------------------------------------------------------
+// Processes Date2 and PatentDate2 as ISOdate formats, computes ms time difference between them
+// --------------------------------------------------------------------------------------------
+
+var totalCitations = db.citations.find().count();
+var then = new Date();
+var processed = 33274687;
+db.citations.find({PatentDate3: {$exists: false}}).noCursorTimeout().forEach(function (citationDoc){
+	
+	citationDoc.PatentDate3 = new Date(citationDoc.PatentDate2);
+	citationDoc.Date3 = new Date(citationDoc.Date2);
+	citationDoc.msDiff = citationDoc.PatentDate3 - citationDoc.Date3;
+
+	db.citations.save(citationDoc)
+	
+	processed += 1;
+	if (processed % 10000 == 0) {
+		now = new Date();-
+		print("Processed: ", processed, "/", totalCitations);
+		print("Time Remaining: ", (totalCitations - processed) * (now - then)/(10000*1000*60));
+		then = now;
+	};
+});
+print("Processed All -------------------------------------------")
+
+// --------------------------------------------------------------------------------------------
+// Computes Patent Link Time and Citation Link Time and difference between them for each Citation
+// --------------------------------------------------------------------------------------------
+
+cursor = db.citations.find({PatentLinkTime: {$exists: false}}).sort({PatentDate})
+cursor = db.citations.find({CitationLinkTime: {$exists: false}}).sort({Date})
+
+var totalPatents = db.patentsOld.count();
+var cursor = db.patentsOld.find({PatentLinkTime: {$exists: true}});
+var PatentLinkTime =  0 //cursor.sort({PatentLinkTime: -1}).limit(1).next().PatentLinkTime;
+var processed = 0; //cursor.count();
+print("Already Processed", processed, "  Max LinkTime: ", PatentLinkTime)
+var then = new Date();
+db.patentsOld.find().noCursorTimeout().forEach(function (patentDoc){ // {PatentLinkTime: {$exists: false}}
+	patentDoc.PatentLinkTime = PatentLinkTime;
+	PatentLinkTime += patentDoc.Order;
+	db.patentsOld.save(patentDoc);
+	
+	processed += 1;
+	if (processed % 10000 == 0) {
+		now = new Date();-
+		print("Processed: ", processed, "/", totalPatents);
+		print("Time Remaining: ", (totalPatents - processed) * (now - then)/(10000*1000*3600));
+		then = now;
+	};
+});
+print("Processed All -------------------------------------------");
+
+
+// ---------------------------------------------------------------------------
+// Sample citations
+// ---------------------------------------------------------------------------
+var start = 19760000
+db.citationsAll.aggregate([
+	{$match: {Citation: 3329015}},
+	{$sample: { size:1000} },
+	{$out: "SampleCitations.1000perYear"}
+])
+
--- Code/mongodb_csv_import.bash
+++ Code/mongodb_csv_import.bash
@@ -6,22 +6,22 @@
         mongoimport -d sotonproject -c patents2 --type csv --file "$f" --headerline
     done
 	
-	#FILES="../DataFiles/Cleaned2/citation/*"
-	#for f in $FILES
-    #do
-    #    mongoimport -d sotonproject -c citations --type csv --file "$f" --headerline
-    #done
+	FILES="../DataFiles/Cleaned2/citation/*"
+	for f in $FILES
+    do
+        mongoimport -d sotonproject -c citations --type csv --file "$f" --headerline
+    done
 	
-	#FILES="../DataFiles/Cleaned/patent/*"
-    #for f in $FILES
-    #do
-    #    echo "$f"
-    #    mongoimport -d sotonproject -c patentsOld --type csv --file "$f" --headerline
-    #done
+	FILES="../DataFiles/Cleaned/patent/*"
+    for f in $FILES
+    do
+        echo "$f"
+        mongoimport -d sotonproject -c patentsOld --type csv --file "$f" --headerline
+    done
 	
-	#FILES="../DataFiles/Cleaned/citation/*"
-	#for f in $FILES
-    #do
-    #    mongoimport -d sotonproject -c citationsOld --type csv --file "$f" --headerline
-    #done
+	FILES="../DataFiles/Cleaned/citation/*"
+	for f in $FILES
+    do
+        mongoimport -d sotonproject -c citationsOld --type csv --file "$f" --headerline
+    done
 	
